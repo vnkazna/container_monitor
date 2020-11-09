7d8b80d3a016a9db41af9287c46a39a2ca05eb96
Tomas Vik <tvik@gitlab.com>
2020-11-09 13:37:19 +0100

    Change file type icon

--- src/data_providers/changed_file_item.js
+++ src/data_providers/changed_file_item.js
@@ -1,14 +1,15 @@
-const { TreeItem, TreeItemCollapsibleState } = require('vscode');
+const { TreeItem, TreeItemCollapsibleState, ThemeIcon, Uri } = require('vscode');
 const path = require('path');
 const { getReviewUri } = require('../utils/get_review_uri');
 
 class ChangedFileItem extends TreeItem {
   constructor(fileDiff, mr, projectUri, headCommit, baseCommit) {
-    super(path.basename(fileDiff.new_path), TreeItemCollapsibleState.None);
+    super(Uri.parse(fileDiff.new_path), TreeItemCollapsibleState.None);
     this.description = path.dirname(fileDiff.new_path);
     this.mr = mr;
     this.projectUri = projectUri;
     this.fileDiff = fileDiff;
+    this.iconPath = ThemeIcon.File;
     const baseFileUri = getReviewUri({
       path: fileDiff.old_path,
       commit: baseCommit,
@@ -19,13 +20,6 @@ class ChangedFileItem extends TreeItem {
       commit: headCommit,
       workspace: projectUri,
     });
-    // const headFileUri = new vscode.Uri(
-    //   REVIEW_URI_SCHEME,
-    //   `authority`,
-    //   `/${fileDiff.new_path}`,
-    //   `commit=${headCommit}&workspace=${encodeURIComponent(projectUri)}`,
-    //   '',
-    // );
     this.command = {
       command: 'vscode.diff',
       arguments: [baseFileUri, headFileUri, path.basename(fileDiff.new_path)],
