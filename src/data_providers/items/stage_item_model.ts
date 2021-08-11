7b0c4fb8d7764a4f9bc51ee6ea73a18566df0a38
Tomas Vik <tvik@gitlab.com>
2021-08-11 10:49:48 +0200

    fix: ci stages not correctly sorted

--- src/data_providers/items/stage_item_model.ts
+++ src/data_providers/items/stage_item_model.ts
@@ -1,18 +1,9 @@
 import * as vscode from 'vscode';
 import { getJobMetadata } from '../../gitlab/ci_status_metadata';
+import { compareBy } from '../../utils/compare_by';
 import { createJobItem } from './create_job_item';
 import { ItemModel } from './item_model';
 
-/** can be passed to Array.sort method, sorts in ascending order */
-const compare = <T>(a: T, b: T): -1 | 0 | 1 => {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  return 0;
-};
-/** creates a comparison method for T objects that can be used for sorting. Can be replaced with lodash sortBy. */
-const compareBy = <T>(key: keyof T) => {
-  return (a: T, b: T) => compare(a[key], b[key]);
-};
 const first = <T>(a: T[]): T | undefined => a[0];
 
 export class StageItemModel extends ItemModel {
