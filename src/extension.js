705a4561b0bf982da9b308635ff799794df17598
Tomas Vik <tvik@gitlab.com>
2021-09-16 11:38:12 +0200

    refactor: move check_version to gitlab namespace
    The check_version is used only in one place and so it doesn't fit the
    utils definition. It relates to GitLab and so it's moved to the gitlab
    namespace.

--- src/extension.js
+++ src/extension.js
@@ -30,7 +30,7 @@ const {
 } = require('./commands/mr_discussion_commands');
 const { hasCommentsDecorationProvider } = require('./review/has_comments_decoration_provider');
 const { changeTypeDecorationProvider } = require('./review/change_type_decoration_provider');
-const { checkVersion } = require('./utils/check_version');
+const { checkVersion } = require('./gitlab/check_version');
 const { checkoutMrBranch } = require('./commands/checkout_mr_branch');
 const { cloneWiki } = require('./commands/clone_wiki');
 const { createSnippetPatch } = require('./commands/create_snippet_patch');
