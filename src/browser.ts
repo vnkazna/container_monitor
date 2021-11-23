44d67ab0a4eaee58f1ae4fc5c721b892910e7a54
Tomas Vik <tvik@gitlab.com>
2021-11-23 13:01:14 +0100

    adding webpack and creating browser extension

---
+++ src/browser.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+import { initializeLogging, log } from './log';
+import { doNotAwait } from './utils/do_not_await';
+
+export const activate = async (context: vscode.ExtensionContext): Promise<void> => {
+  const outputChannel = vscode.window.createOutputChannel('GitLab Workflow');
+  initializeLogging(line => outputChannel.appendLine(line));
+  log('started');
+  doNotAwait(vscode.window.showInformationMessage('Extension in the browser'));
+};
