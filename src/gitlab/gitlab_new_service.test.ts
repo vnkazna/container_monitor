c51eadd5d9a7455feb82cd5720342dadc8c91670
Tomas Vik <tvik@gitlab.com>
2021-11-25 13:16:30 +0100

    full test suite

--- src/gitlab/gitlab_new_service.test.ts
+++ src/gitlab/gitlab_new_service.test.ts
@@ -141,20 +141,9 @@ describe('gitlab_new_service', () => {
     });
   });
   describe('fetchIssueables', () => {
-    const TEST_REPOSITORY = {
-      remote: {
-        host: 'TEST_HOST',
-        namespace: 'TEST_NS',
-        project: 'TEST_PROJECT',
-      },
-      getProject: () => ({ groupRestId: 'TEST_PROJECT', restId: 'TEST_PROJECT' }),
-    };
-
-    let repository: any;
     let gitLabService: GitLabNewService;
 
     beforeEach(() => {
-      repository = TEST_REPOSITORY;
       gitLabService = new GitLabNewService(`http://gitlab.example.com`);
       asMock(crossFetch).mockResolvedValue(crossFetchResponse([]));
       asMock(getExtensionConfiguration).mockReturnValue({});
@@ -164,14 +153,7 @@ describe('gitlab_new_service', () => {
       jest.resetAllMocks();
     });
 
-    // These are required, but not used in the function
-    const baseParams = {
-      name: '',
-      noItemText: '',
-    };
-
-    // These are the other parameters used
-    const defaultParams = {
+    const defaultParams: CustomQuery = {
       name: 'test query',
       noItemText: 'no items',
       type: CustomQueryType.MR,
@@ -179,7 +161,6 @@ describe('gitlab_new_service', () => {
       state: 'opened',
       wip: '',
       searchIn: '',
-      pipelineId: undefined,
       confidential: false,
       excludeLabels: undefined,
       excludeMilestone: undefined,
@@ -201,29 +182,6 @@ describe('gitlab_new_service', () => {
       severityLevels: undefined,
       confidenceLevels: undefined,
     };
-
-    let request: jest.Mock;
-    let fetchIssuablesRef: (params: CustomQuery, repositoryRoot: string) => Promise<RestIssuable[]>;
-
-    interface SetupFetchOptions {
-      version?: string;
-      responses?: unknown[];
-    }
-
-    // const setupFetchIssuable = ({ version = '13.9', responses = [] }: SetupFetchOptions = {}) => {
-    //   request = require('request-promise');
-    //   const { fetchIssuables } = require('./gitlab_service');
-    //   jest.mock('request-promise', () => jest.fn(() => ({ response: [] })));
-    //   fetchIssuablesRef = fetchIssuables;
-
-    //   const createResponseImplementation = (response: unknown) => () => ({ response });
-    //   const implementations = [{ version }, ...responses].map(createResponseImplementation);
-
-    //   implementations.forEach(imp => request.mockImplementationOnce(imp));
-    // };
-
-    // const fetchIssuablesHelper = (params: Partial<CustomQuery> = {}) =>
-    //   fetchIssuablesRef({ ...baseParams, ...defaultParams, ...params }, 'test');
 
     const getMockCall = (calls: any[], pattern: string) =>
       calls.find((call: string[]) => call[0].includes(pattern));
@@ -307,90 +265,81 @@ describe('gitlab_new_service', () => {
       expect(search.get('reviewer_username')).toEqual('reviewer');
     });
 
-    //   describe('searchIn parameters', () => {
-    //     it('sets "all" parameter', async () => {
-    //       setupFetchIssuable();
-    //       await fetchIssuablesHelper({ searchIn: 'all' });
-    //       const search = new URLSearchParams(request.mock.calls[1][0]);
-    //       expect(search.get('in')).toEqual('title,description');
-    //     });
+    describe('searchIn parameters', () => {
+      it('sets "all" parameter', async () => {
+        await gitLabService.getIssuables({ ...defaultParams, searchIn: 'all' }, project);
+        const search = new URLSearchParams(asMock(crossFetch).mock.calls[0][0]);
+        expect(search.get('in')).toEqual('title,description');
+      });
 
-    //     it('sets "in" parameter', async () => {
-    //       setupFetchIssuable();
-    //       await fetchIssuablesHelper({ searchIn: 'title' });
-    //       const search = new URLSearchParams(request.mock.calls[1][0]);
-    //       expect(search.get('in')).toEqual('title');
-    //     });
-    //   });
+      it('sets "in" parameter', async () => {
+        await gitLabService.getIssuables({ ...defaultParams, searchIn: 'title' }, project);
+        const search = new URLSearchParams(asMock(crossFetch).mock.calls[0][0]);
+        expect(search.get('in')).toEqual('title');
+      });
+    });
 
-    //   describe('WIP/Draft', () => {
-    //     it('sets wip parameter', async () => {
-    //       setupFetchIssuable();
-    //       await fetchIssuablesHelper({ wip: 'true' });
-    //       const search = new URLSearchParams(request.mock.calls[1][0]);
-    //       expect(search.get('wip')).toEqual('true');
-    //     });
-    //   });
+    describe('WIP/Draft', () => {
+      it('sets wip parameter', async () => {
+        await gitLabService.getIssuables({ ...defaultParams, wip: 'true' }, project);
+        const search = new URLSearchParams(asMock(crossFetch).mock.calls[0][0]);
+        expect(search.get('wip')).toEqual('true');
+      });
+    });
 
-    //   describe('misc query parameters', () => {
-    //     it('sets query parameters', async () => {
-    //       setupFetchIssuable();
-    //       await fetchIssuablesHelper({
-    //         type: CustomQueryType.ISSUE,
-    //         confidential: true,
-    //         excludeLabels: ['label1', 'label2'],
-    //         excludeMilestone: 'excludeMilestone',
-    //         excludeAuthor: 'excludeAuthor',
-    //         excludeAssignee: 'excludeAssignee',
-    //         excludeSearch: 'excludeSearch',
-    //         excludeSearchIn: 'excludeSearchIn',
-    //         labels: ['label1', 'label2'],
-    //         milestone: 'milestone',
-    //         search: 'search',
-    //         createdBefore: 'createdBefore',
-    //         createdAfter: 'createdAfter',
-    //         updatedBefore: 'updatedBefore',
-    //         updatedAfter: 'updatedAfter',
-    //         orderBy: 'orderBy',
-    //         sort: 'sort',
-    //         maxResults: 20,
-    //         reportTypes: ['reportType1', 'reportType2'],
-    //         severityLevels: ['severityLevel1', 'severityLevel2'],
-    //         confidenceLevels: ['confidenceLevel1', 'confidenceLevel2'],
-    //       });
-    //       const search = new URLSearchParams(request.mock.calls[1][0]);
+    describe('misc query parameters', () => {
+      it('sets query parameters', async () => {
+        await gitLabService.getIssuables(
+          {
+            ...defaultParams,
+            type: CustomQueryType.ISSUE,
+            confidential: true,
+            excludeLabels: ['label1', 'label2'],
+            excludeMilestone: 'excludeMilestone',
+            excludeAuthor: 'excludeAuthor',
+            excludeAssignee: 'excludeAssignee',
+            excludeSearch: 'excludeSearch',
+            excludeSearchIn: 'excludeSearchIn',
+            labels: ['label1', 'label2'],
+            milestone: 'milestone',
+            search: 'search',
+            createdBefore: 'createdBefore',
+            createdAfter: 'createdAfter',
+            updatedBefore: 'updatedBefore',
+            updatedAfter: 'updatedAfter',
+            orderBy: 'orderBy',
+            sort: 'sort',
+            maxResults: 20,
+            reportTypes: ['reportType1', 'reportType2'],
+            severityLevels: ['severityLevel1', 'severityLevel2'],
+            confidenceLevels: ['confidenceLevel1', 'confidenceLevel2'],
+          },
+          project,
+        );
+        const search = new URLSearchParams(asMock(crossFetch).mock.calls[0][0]);
 
-    //       expect(search.get('confidential')).toEqual('true');
-    //       expect(search.get('not[labels]')).toEqual('label1,label2');
-    //       expect(search.get('not[milestone]')).toEqual('excludeMilestone');
-    //       expect(search.get('not[author_username]')).toEqual('excludeAuthor');
-    //       expect(search.get('not[assignee_username]')).toEqual('excludeAssignee');
-    //       expect(search.get('not[search]')).toEqual('excludeSearch');
-    //       expect(search.get('not[in]')).toEqual('excludeSearchIn');
-    //       expect(search.get('labels')).toEqual('label1,label2');
-    //       expect(search.get('milestone')).toEqual('milestone');
-    //       expect(search.get('search')).toEqual('search');
-    //       expect(search.get('created_before')).toEqual('createdBefore');
-    //       expect(search.get('created_after')).toEqual('createdAfter');
-    //       expect(search.get('updated_before')).toEqual('updatedBefore');
-    //       expect(search.get('updated_after')).toEqual('updatedAfter');
-    //       expect(search.get('order_by')).toEqual('orderBy');
-    //       expect(search.get('sort')).toEqual('sort');
-    //       expect(search.get('per_page')).toEqual('20');
-    //       expect(search.get('report_type')).toEqual('reportType1,reportType2');
-    //       expect(search.get('severity')).toEqual('severityLevel1,severityLevel2');
-    //       expect(search.get('confidence')).toEqual('confidenceLevel1,confidenceLevel2');
-    //     });
-    //   });
-
-    //   describe('pipeline parameters', () => {
-    //     it('sets pipeline_id when given a number', async () => {
-    //       setupFetchIssuable();
-    //       await fetchIssuablesHelper({ pipelineId: 1 });
-    //       const search = new URLSearchParams(request.mock.calls[1][0]);
-    //       expect(search.get('pipeline_id')).toEqual('1');
-    //     });
-    //   });
+        expect(search.get('confidential')).toEqual('true');
+        expect(search.get('not[labels]')).toEqual('label1,label2');
+        expect(search.get('not[milestone]')).toEqual('excludeMilestone');
+        expect(search.get('not[author_username]')).toEqual('excludeAuthor');
+        expect(search.get('not[assignee_username]')).toEqual('excludeAssignee');
+        expect(search.get('not[search]')).toEqual('excludeSearch');
+        expect(search.get('not[in]')).toEqual('excludeSearchIn');
+        expect(search.get('labels')).toEqual('label1,label2');
+        expect(search.get('milestone')).toEqual('milestone');
+        expect(search.get('search')).toEqual('search');
+        expect(search.get('created_before')).toEqual('createdBefore');
+        expect(search.get('created_after')).toEqual('createdAfter');
+        expect(search.get('updated_before')).toEqual('updatedBefore');
+        expect(search.get('updated_after')).toEqual('updatedAfter');
+        expect(search.get('order_by')).toEqual('orderBy');
+        expect(search.get('sort')).toEqual('sort');
+        expect(search.get('per_page')).toEqual('20');
+        expect(search.get('report_type')).toEqual('reportType1,reportType2');
+        expect(search.get('severity')).toEqual('severityLevel1,severityLevel2');
+        expect(search.get('confidence')).toEqual('confidenceLevel1,confidenceLevel2');
+      });
+    });
   });
 });
 
