54ed3e1331044039676d2f0233392f5bf3f181a8
Tomas Vik <tvik@gitlab.com>
2022-03-06 12:51:37 +0100

    feat: validate instance URL for new token

--- src/token_input.ts
+++ src/token_input.ts
@@ -2,13 +2,27 @@ import vscode from 'vscode';
 import { GITLAB_COM_URL } from './constants';
 import { tokenService } from './services/token_service';
 
+export const validateInstanceUrl = (input: string): string | undefined => {
+  if (!/^https?:\/\/.*$/.test(input)) return 'Must begin with http:// or https://';
+  try {
+    // eslint-disable-next-line no-new
+    new URL(input);
+  } catch (e) {
+    return 'Must be a valid URL';
+  }
+  return undefined;
+};
+
 export async function showInput() {
   const instance = await vscode.window.showInputBox({
     ignoreFocusOut: true,
     value: GITLAB_COM_URL,
     placeHolder: 'E.g. https://gitlab.com',
     prompt: 'URL to Gitlab instance',
+    validateInput: validateInstanceUrl,
   });
+
+  if (!instance) return;
 
   const token = await vscode.window.showInputBox({
     ignoreFocusOut: true,
@@ -16,9 +30,9 @@ export async function showInput() {
     placeHolder: 'Paste your GitLab Personal Access Token...',
   });
 
-  if (instance && token) {
-    await tokenService.setToken(instance, token);
-  }
+  if (!token) return;
+
+  await tokenService.setToken(instance, token);
 }
 
 export async function removeTokenPicker() {
